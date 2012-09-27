390962969530ac433819bcb261c6348d384d95c8
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-09-27 18:55:47 +0200

    Retrieve all href info in a objects from http://es.wikipedia.org

--- ricardo-crawler.py
+++ ricardo-crawler.py
@@ -7,12 +7,18 @@ Created on 27/09/2012
 import urllib2
 from setuptools.package_index import user_agent
 from urllib2 import _opener
+from BeautifulSoup import BeautifulSoup as Soup
 
 user_agent = "Mozilla/5.0 (X11; U; Linux x86_64; en-Us) AppleWebKit/534.7 (KHTML, Like Gecko) Chrome /7.0.517.41 Safari/534.7"
 
 _opener = urllib2.build_opener()
-_opener.addheaders = [('User-agent',user_agent)]
+_opener.addheaders = [('User-agent', user_agent)]
 
-raw_code = _opener.open("http://es.wikipedia.org/robots.txt").read()
+raw_code = _opener.open("http://es.wikipedia.org").read()
 
-print raw_code
+soup_code = Soup(raw_code)
+links = [link['href']  for link
+         in soup_code.findAll('a')
+         if link.has_key('href')]
+
+print links
