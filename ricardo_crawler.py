af6e8b28a92ab4eea4328025cfe67585d7a22f6c
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-10-02 23:29:02 +0200

    #6159 - Recursive crawler solution Convert script into object class

--- ricardo_crawler.py
+++ ricardo_crawler.py
@@ -8,27 +8,51 @@ Created on 27/09/2012
 
 import argparse
 from controllers.http_links_collector import HttpLinksCollector
+
+class RicardoCrawler:
+    '''RicardoCrawler class to crawl a web using system parameters.
+    
+    '''
+    
+    def __init__(self):
+        '''Init method
+        '''
         
-# ArgParse definition rules
-PARSER = argparse.ArgumentParser(description="Let's crawl a web")
+        pass
 
-PARSER.add_argument('url', nargs=1, help='target URL')
-PARSER.add_argument('-n', '--number-of-levels', type = int, \
-                    default = 1, help = 'how depth the crawl will go.')
+    def crawler_start(self):
+        '''Method to start crawling.
+            * Checks input parameters.
+            * returns the result of crawling printing a dictionary on the screen.
+        
+        '''
 
-ARGS = PARSER.parse_args()
+        # ArgParse definition rules
+        parser = argparse.ArgumentParser(description="Let's crawl a web")
+        
+        parser.add_argument('url', nargs=1, help='target URL')
+        parser.add_argument('-n', '--number-of-levels', type = int, \
+                            default = 1, help = 'how depth the crawl will go.')
+        
+        # Create argument object
+        args = parser.parse_args()
+        
+        target_url = args.url.pop()
+        depth = args.number_of_levels
+        
+        # Starting level to retrieve links
+        level = 1
+        links = {}
+        http_links_collector = HttpLinksCollector(target_url)
+        
+        links_list = http_links_collector.\
+            retrieve_links(target_url, depth, level)
+        
+        links[target_url] = links_list
+        
+        print links
 
-TARGET_URL = ARGS.url.pop()
-DEPTH = ARGS.number_of_levels
-
-# Starting level to retrieve links
-LEVEL = 1
-LINKS = {}
-HTTPLINKSCOLLECTOR = HttpLinksCollector(TARGET_URL)
-
-LINKS_LIST = HTTPLINKSCOLLECTOR.retrieve_links(TARGET_URL, DEPTH, LEVEL)
-
-LINKS[TARGET_URL] = LINKS_LIST
-
-print LINKS
-
+if __name__ == '__main__':
+    RC = RicardoCrawler()
+    RC.crawler_start()
+        
