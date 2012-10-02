a4e12fccb4eb665eb79163dd89f13bef19f1fea5
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-10-02 12:32:51 +0200

    #6160 - Documentation Your code has been rated at 10.00/10 (previous run: 9.33/10)

--- ricardo_crawler.py
+++ ricardo_crawler.py
@@ -10,24 +10,25 @@ import argparse
 from controllers.http_links_collector import HttpLinksCollector
         
 # ArgParse definition rules
-parser = argparse.ArgumentParser(description="Let's crawl a web")
+PARSER = argparse.ArgumentParser(description="Let's crawl a web")
 
-parser.add_argument('url', nargs=1, help='target URL')
-parser.add_argument('-n', '--number-of-levels', type=int, default=1, help='how depth the crawl will go.')
+PARSER.add_argument('url', nargs=1, help='target URL')
+PARSER.add_argument('-n', '--number-of-levels', type = int, \
+                    default = 1, help = 'how depth the crawl will go.')
 
-args = parser.parse_args()
+ARGS = PARSER.parse_args()
 
-target_url = args.url.pop()
-depth = args.number_of_levels
+TARGET_URL = ARGS.url.pop()
+DEPTH = ARGS.number_of_levels
 
 # Starting level to retrieve links
-level = 1;
-links = {}
-httpLinksCollector = HttpLinksCollector(target_url)
+LEVEL = 1
+LINKS = {}
+HTTPLINKSCOLLECTOR = HttpLinksCollector(TARGET_URL)
 
-links_list = httpLinksCollector.retrieve_links(target_url, depth, level)
+LINKS_LIST = HTTPLINKSCOLLECTOR.retrieve_links(TARGET_URL, DEPTH, LEVEL)
 
-links[target_url] = links_list
+LINKS[TARGET_URL] = LINKS_LIST
 
-print links
+print LINKS
 
