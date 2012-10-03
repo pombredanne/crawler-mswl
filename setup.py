a7b228ffa4e46e835e4a9a5ed95406af80a033f4
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-10-03 19:05:51 +0200

    #6160 - Documentation
    License GNU v3 added to project.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+# coding=UTF-8
 '''
 Created on 27/09/2012
 
 Setuptools setup file
 
-@author: ricardo
+@author: Ricardo García Fernández
+@mail: ricardogarfe@gmail.com
 '''
  
 from setuptools import setup, find_packages
 
 setup (name="ricardo_crawler",
-        version="0.1",
+        version="1.0.0",
         packages=find_packages(),
         scripts=['ricardo_crawler'],
         install_requires=['BeautifulSoup'],
@@ -17,9 +19,9 @@ setup (name="ricardo_crawler",
         author='Ricardo García Fernández',
         author_email='ricardogarfe@gmail.com',
         description='My first Web Scrapper in Python',
-        license='',
-        keywords='',
-        url='',
-        long_description='',
+        license = 'GNU GPLv3',
+        keywords='python, web-crawler',
+        url='https://github.com/ricardogarfe/crawler-mswl',
+        long_description='Web Scrapper in Python for Development Tools Subject from MSWL - URJC',
         download_url='',
         )
