882bb7d7e5c631dc277f27d31470ecece4143dac
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-10-03 19:10:13 +0200

    #6159 - Recursive crawler solution
    Test to avoid forbidden urls.

--- test/controllers/TestHttpLinksCollector.py
+++ test/controllers/TestHttpLinksCollector.py
@@ -1,12 +1,33 @@
+# coding=UTF-8
 '''
-Created on 01/10/2012
+    This file is part of crawler-mswl.
 
-@author: ricardo
+    crawler-mswl is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    crawler-mswl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with crawler-mswl.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
+
 import unittest
+from controllers.http_links_collector import HttpLinksCollector
 
 class TestHttpLinksCollector (unittest.TestCase):
     '''Test class for methods defined in HttpLinksCollector class.
+    
+    Created on 01/10/2012
+
+    @author: Ricardo García Fernández
+    @mail: ricardogarfe@gmail.com
+
     '''
 
     def setUp(self):
@@ -16,7 +37,19 @@ class TestHttpLinksCollector (unittest.TestCase):
         pass
 
     def test_retrieveLinks(self):
-        pass
+        '''Test method for HttpLinksCollector.retrieve_links.
+        
+        '''
+        
+        # Test 1 - check HttpError 401 in log
+        starting_url = "http://www.nature.com"
+        target_url = "http://www.nature.com/nature/journal/v438/n7070/full/438900a.html"
+
+        http_links_collector = HttpLinksCollector(starting_url)        
+        links_retrieved = http_links_collector.retrieve_links(target_url)
+        
+        self.assertTrue(not links_retrieved, \
+                       "Retreved Links from:'" + target_url + "'")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testRetrieveLinks']
--- test/controllers/TestUrlContentRetrieve.py
+++ test/controllers/TestUrlContentRetrieve.py
@@ -1,14 +1,34 @@
+# coding=UTF-8
 '''
-Created on 01/10/2012
+    This file is part of crawler-mswl.
 
-@author: ricardo
+    crawler-mswl is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    crawler-mswl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with crawler-mswl.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
+
 import unittest
 from controllers.url_content_retrieve import UrlContentRetrieve
 from urllib2 import URLError
 
 class TestUrlContentRetrieve(unittest.TestCase):
     '''Test class for methods defined in UrlContentRetrieve class.
+    
+    Created on 01/10/2012
+
+    @author: Ricardo García Fernández
+    @mail: ricardogarfe@gmail.com
+
     '''
 
     def setUp(self):
